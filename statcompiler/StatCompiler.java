80ac849266c099d2b52ed78f0ac0aef02d211912
Pratik Doshi <51810841+Pratik-Doshi-99@users.noreply.github.com>
2020-04-28 17:17:44 +0530

    The MainClass of the JAR file

---
+++ statcompiler/StatCompiler.java
@@ -0,0 +1,18 @@
+package statcompiler;
+
+/**
+ *
+ * @author Pratik Doshi
+ */
+public class StatCompiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame f = new MainFrame();
+        f.main(null);
+    }
+    
+}
