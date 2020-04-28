d4a8d358b4cfadd5bc1f2c1f4c4a9676ce8d38c2
Pratik Doshi <51810841+Pratik-Doshi-99@users.noreply.github.com>
2020-04-28 17:12:57 +0530

    Updated ServerConnect to Version 2.0
    Earlier ServerConnect was version 1.0 by mistake

--- core/ServerConnect.java
+++ core/ServerConnect.java
@@ -1,6 +1,5 @@
 package core;
 
-import Practise.ICICIDirect;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,9 +7,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Calendar;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -18,48 +14,86 @@ import java.util.logging.Logger;
  */
 
 public class ServerConnect {
+    /**
+     * @param args the command line arguments
+     */
+    private DynamicCalculation DYNAMIC;
+    public ServerConnect(){
+        this.DYNAMIC = null;
+    }
+    public ServerConnect(DynamicCalculation dynamic){
+        this.DYNAMIC = dynamic;
+    }
     
-    public static void getCSV(QueryParameter param, File target){
+    public void getCSV(QueryParameter param, File target){
         try {
             URL url = new URL(param.getQuery());
             URLConnection conn = url.openConnection();
             conn.setRequestProperty("Cookie",param.getCookie());
             conn.connect();
             InputStreamReader ir = new InputStreamReader(conn.getInputStream(),"UTF-8");
-            FileOutputStream fos = new FileOutputStream(target);
-            int i =0;
-            fos.write((param.getSymbol() + "\n").getBytes());
-            while((i = ir.read())>=0){
-                fos.write((byte)i);
+            FileOutputStream fos = null;
+            if(target != null){
+                fos = new FileOutputStream(target);
+                fos.write((param.getSymbol() + "\n").getBytes());
             }
-            fos.close();
+            int i =0;
+            String row = "";
+            while((i = ir.read())>=0){
+                if(fos != null){
+                    fos.write((byte)i);
+                }
+                if((char)i == '\n' && this.DYNAMIC != null){
+                    this.DYNAMIC.calculate(row);
+                    row = "";
+                    continue;
+                }
+                if(this.DYNAMIC != null){
+                    row += (char)i;
+                }
+                
+            }
+
+            if(fos != null){
+                fos.close();
+            }
+            if(this.DYNAMIC != null){
+                this.DYNAMIC.calculate(row);
+            }
             ir.close();
             
         } catch (MalformedURLException ex) {
-            Logger.getLogger(ICICIDirect.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         } catch (IOException ex) {
             System.out.println("Error: " +  ex.toString());
+            ex.printStackTrace();
         } catch (NullPointerException ex){
-            Logger.getLogger(ICICIDirect.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
         
     }
     
- 	//testing the code
+ 
     public static void main(String[] args) {
         
         try {
-            File f = new File("C:\\Users\\Pratik Doshi\\Desktop\\CSV5.csv");
-            QueryParameter param = new QueryParameter("CGPOWER.NS",
-                    new Date(2,7,2019),
-                    new Date(15,7,2019),
+            File f = new File("Z:\\Desktop\\CSV6.csv");
+            QueryParameter param = new QueryParameter("ARVIND.NS",
+                    new Date(1,7,2018),
+                    new Date(17,7,2018),
                     Interval.DAILY);
-            ServerConnect.getCSV(param, f);
+            //new ServerConnect().getCSV(param, f);
             param.setSymbol("SBIN.NS");
-            ServerConnect.getCSV(param, f);
+            //new ServerConnect().getCSV(param, f);
+            DynamicCalculation calc = (String row) -> System.out.println(row);
+            new ServerConnect(calc).getCSV(param,f);
+            
+            
         } catch (InvalidDateException ex) {
             System.out.println(ex.getMessage());
         }
         
-    } 
+    }
+    
+    
 }
